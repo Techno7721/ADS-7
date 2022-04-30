066b6def2b41c4e03908d65cef54062df9375245
Осинин Ярослав <99194733+Techno7721@users.noreply.github.com>
2022-04-30 20:10:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,58 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Item {
+    T value;
+    Item* next;
+    Item* prev;
+  };
+  Item* head=nullptr, * tail=nullptr;
+  TPQueue<T>::Item* create(T value) {
+    Item* item = new Item;
+    item->value = value;
+    item->next = nullptr;
+    item->prev = nullptr;
+    return item;
+  }
+
+ public:
+  void push(T value) {
+    Item* temp = create(value);
+    if (head && tail) {
+      if ((head->value.prior) < (temp->value.prior)) {
+        head->prev = temp;
+        temp->next = head;
+        head = temp;
+      } else if ((tail->value.prior) >= (temp->value.prior)) {
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+      } else {
+        Item* temphead = head;
+        while (true) {
+          if ((temphead->next->value.prior) < (temp->value.prior)) {
+            temp->next = temphead->next;
+            temp->prev = temphead;
+            temphead->next = temp;
+            break;
+          }
+          temphead = temphead->next;
+        }
+      }
+    } else {
+        head = tail = create(value);
+    }
+  }
+  T pop() {
+    if (head && tail) {
+      Item* temp = head->next;
+      T value = head->value;
+      delete head;
+      head = temp;
+      return value;
+    }
+  }
 };
 
 struct SYM {
